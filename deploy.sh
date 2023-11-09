aa906cf98e5c14e9ca23e75b8f934cf78f8a3db8
Richard <rhshadowgreen@gmail.com>
2023-11-09 14:27:58 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,19 @@
 #! /bin/bash
 
 # Arguments
-# 1 GITHUB_USER
-# 2 REPO_NAME
+# 1 REPO_OWNER
+# 2 REPO_URL
 
 # Command
-# ssh -i ~/.ssh/id_rsa root@host "bash -s" < deploy.sh GITHUB_USER REPO_NAME
-# TEST: ssh -i ~/.ssh/id_rch root@linode4.rch.app "bash -s" < deploy.sh strawstack heylisten
+# ssh -i ~/.ssh/id_rsa root@host "bash -s" < deploy.sh REPO_OWNER REPO_URL
 
 git config --global user.name "Github Actions"
 git config --global user.email "actions@github.com"
 
-rm -rf $2
+rm -rf repo
 
-git clone https://github.com/$1/$2.git
-cd $2
+git clone $2 repo
+cd repo
 
 npm install
 pm2 restart server.mjs
